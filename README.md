6c5caf8b1f3b20bf0f72662ea0a41357c0713852
Emil Tin <zf0f@tmf.kk.dk>
2019-02-13 13:41:21 +0100

    improved queries

--- README.md
+++ README.md
@@ -19,20 +19,14 @@ AG (intersection), object type, manufacturer and number.
 
 ## Input
 A .csv file exported from MobiMaestro, containing events about connects and disconnects.
-The first is expected to contain a CVS header.
+The file is expected to contain a CVS header.
 The file must be located at data/events.csv
 
-The Postgres lag() function to compute the
-duration between consecutive pairs of disconnect-connect pairs.
-Disconnects are expected to be followed by a connect. If several disconnects
-occur without a connect inbetween, then only the last disconnect-connect
-duration will be used.
+From MobiMaestro, select System > Event History. Select the "Disconnect analysis" filter and laod it.
+Make sure the following columns are shown, and nothing else, and in the correct order:
+	Timestamp, Object type, Object, Event, Description.
 
-Computing uptime percentage requies knowledge about the total duration of the dataset.
-This is currently assumed to be 30 days. When you export data from MobiMaestro,
-you need to ensure the data actually covers 30 days, or you will have to adjust
-the total time used in analyse.sql.
-
+Then click "Search", and then "Export results..." to save as a .csv file.
 
 ## Output
 A .csv file with the result.
@@ -40,19 +34,31 @@ The file will contain a CSV header.
 The file will be located at data/result.csv
 
 ## Usage
-You must have docker desktop installed first:
+
+### Prerequisites
+You must have docker desktop installed:
 https://www.docker.com/products/docker-desktop
 
+### Dataset time range
+Computing uptime percentage requires knowledge about the time range of the dataset.
+
+All datasets are expected to cover whole months. Make sure this is true when you export from MobiMaestro.
+
+You must set the total number of days the dataset cover in the analyze.sql file.
+
+
 ### Just give me the CSV output
-Place the input data in data/event.csv, and run:
-$ docker run -v `pwd`/data:/docker-entrypoint-initdb.d --rm postgres
+Run:
+docker run -v `pwd`/data:/docker-entrypoint-initdb.d --rm postgres
+
 Press Control-C to stop the container.
-The output will be locatd in data/result.csv.
+The output will be located in data/result.csv.
+
 
 ### Running custom SQL queries
-Place the input data in data/event.csv, and run:
-$ docker run -v `pwd`/data:/docker-entrypoint-initdb.d --rm -d postgres > container_id
-$ docker exec -it `cat container_id` psql -U postgres
+Run:
+docker run -v `pwd`/data:/docker-entrypoint-initdb.d --rm -d postgres > container_id
+docker exec -it `cat container_id` psql -U postgres
 
 Now you're in PostgreSQL, with raw data already import to the "events" table,
 and statistics in the "stats" table. You can do any SQL queries you like, eg:
@@ -65,9 +71,11 @@ When done, you might want to exit sql, and perhaps exit the container and stop i
 
 postgres=# \q
 root@71364c7e3116 :/# exit
-$ docker stop `cat container_id`
+$ docker stop `cat container_id`; rm container_id;
 
-## Details
+## Notes
+
+### Docker files
 The tool uses a docker container to run PostgreSQL and perform the analysis. 
 The folder data/ is mounted as a volume to read the input and save the output.
 The volume is mounted at /docker-entrypoint-initdb.d. Any .sql files in
@@ -77,4 +85,9 @@ is started.
 The file data/analys.sql contains the SQL commands to import the csv file, run
 the analysis and store the result in a csv file.
 
-
+### Uptime computation
+The Postgres lag() function is used to compute the
+duration between consecutive pairs of disconnect-connect pairs.
+Disconnects are expected to be followed by a connect. If several disconnects
+occur without a connect inbetween, then only the last disconnect-connect
+duration will be used.
--- data/analyse.sql
+++ data/analyse.sql
@@ -1,10 +1,12 @@
 
+
 -- create table
 DROP TABLE IF EXISTS events;
 CREATE TABLE events
 (
   id serial NOT NULL,
   at timestamp,
+  type character varying(255),
   object character varying(255),
   event character varying(255),
   description character varying(255),
@@ -12,40 +14,117 @@ CREATE TABLE events
 );
 
 -- import from CSV
-COPY events(at,object,event,description) FROM '/docker-entrypoint-initdb.d/events.csv' DELIMITER ';' CSV HEADER;
+COPY events(at,type,object,event,description) FROM '/docker-entrypoint-initdb.d/events.csv' DELIMITER ';' CSV HEADER;
 
--- analyze and output to table
+
+
+-- find pairs of consecutive disconnect-connect pairs for each device
+DROP TABLE IF EXISTS downtime;
 SELECT
-object,
-split_part(object,'-',2) AS ag,
-split_part(object,'-',3) AS type,
-split_part(object,'-',4) AS manufacturer,
-split_part(object,'-',5) AS nr,
-COUNT(duration) AS num,
-ROUND( CAST( 100-(100*date_part('epoch', SUM(duration))/(60*60*24*30)) AS numeric), 2) AS uptime,
-date_part('epoch', SUM(duration)) AS sum,
-ROUND(date_part('epoch',AVG(duration))) AS avg,
-date_part('epoch', MAX(duration)) AS max,
-ROUND( CAST( stddev_samp(EXTRACT(EPOCH FROM duration)) AS numeric), 2) AS std_dev
-INTO TABLE stats
+*
+INTO TABLE downtime
 FROM
 	(SELECT
 	id,
+	type,
 	object,
-	lag(at, 1) OVER (PARTITION BY object ORDER BY at) AS starting,
+	DATE_PART('year',at) AS year,
+	DATE_PART('month',at) AS month,
+	split_part(object,'-',2) AS ag,
+	split_part(object,'-',3) AS type_code,
+	split_part(object,'-',4) AS manufacturer,
+	split_part(object,'-',5) AS nr,	
+	lag(at, 1) OVER w AS starting,
 	at AS ending,
-	at - lag(at, 1) OVER (PARTITION BY object ORDER BY at) AS duration,
-	event,
-	lag(event, 1) OVER (PARTITION BY object ORDER BY at) AS prev_event
+	justify_interval(at - lag(at, 1) OVER w) AS duration,
+	lag(event, 1) OVER w AS prev_event,
+	event
 	FROM events
+	WINDOW w AS (PARTITION BY object ORDER BY at)
 	ORDER BY object, at) AS disconnects
-WHERE duration IS NOT NULL AND
-event = 'Connected' AND
-prev_event LIKE '%Disconnected%'
-GROUP BY object
-ORDER BY ag DESC;
+WHERE 
+duration IS NOT NULL AND
+prev_event IN (
+	'Disconnected due to fatal error',
+	'No communication',
+	'Disconnected',
+	'Connection problem',
+	'RSU communication error',
+	'RSU not available'
+	'Communication failed',
+	'Connection problem',
+	'Connection stopped',
+	'Connection error',
+	'Connect error'
+) AND
+event IN (
+	'Connected',
+	'Connection started',
+	'Ok',
+	'RSU ok'
+); 
+
+
+-- group by device
+DROP TABLE IF EXISTS by_device;
+SELECT
+type,
+object,
+ag,
+type_code,
+manufacturer,
+nr,
+COUNT(duration) AS num,
+ROUND( CAST( 100-(100*date_part('epoch', SUM(duration))/(60*60*24*181)) AS numeric), 2) AS uptime,
+justify_interval(SUM(duration)) AS sum,
+justify_interval(AVG(duration)) AS avg,
+justify_interval(MAX(duration)) AS max
+INTO TABLE by_device
+FROM downtime
+GROUP BY type,object,ag,type_code,manufacturer, nr;
+
+
+-- group by device type
+DROP TABLE IF EXISTS by_type;
+SELECT
+type,
+COUNT(DISTINCT object) as devices,
+ROUND( CAST( 100-(100*date_part('epoch', SUM(duration))/(60*60*24*181*COUNT(DISTINCT object))) AS numeric), 2) AS uptime,
+COUNT(type) AS disconnects,
+justify_interval(AVG(duration)) AS avg_duration,
+justify_interval(MAX(duration)) AS max_duration
+INTO TABLE by_type
+FROM downtime
+GROUP BY type;
+
+-- group by months and type
+DROP TABLE IF EXISTS by_month;
+SELECT
+year,
+month,
+type,
+COUNT(DISTINCT object) as devices,
+ROUND(
+	CAST(
+		100-(100*
+			date_part('epoch', SUM(duration))/ (		-- total downtime in seconds
+			60*60*24 * -- seconds per day
+			extract(days FROM make_date(year::INTEGER,month::INTEGER,1) + interval '1 month' - interval '1 day') * -- days in month
+			COUNT(DISTINCT object) -- number of devices
+		)
+	) AS numeric),
+	2
+) AS uptime,
+COUNT(type) AS disconnects,
+justify_interval(AVG(duration)) AS avg_duration,
+justify_interval(MAX(duration)) AS max_duration
+INTO TABLE by_month
+FROM downtime
+GROUP BY year,month,type
+ORDER BY type,year,month;
+
 
 -- export to CSV
-COPY (SELECT * from stats ORDER BY ag)TO '/docker-entrypoint-initdb.d/result.csv' DELIMITER ',' CSV HEADER;
-
-
+COPY (SELECT * from by_device ORDER BY type,manufacturer,uptime ASC) TO '/docker-entrypoint-initdb.d/by_device.csv' DELIMITER ',' CSV HEADER;
+COPY (SELECT * from by_type ORDER BY type) TO '/docker-entrypoint-initdb.d/by_type.csv' DELIMITER ',' CSV HEADER;
+COPY (SELECT * from by_month ORDER BY type,year,month) TO '/docker-entrypoint-initdb.d/by_month.csv' DELIMITER ',' CSV HEADER;
